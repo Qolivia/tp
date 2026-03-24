aed20a7d095dcf58a1b71d02319da4eac3a6251f
Daren Tan <daren28@gmail.com>
2026-03-24 17:21:31 +0800

    Implement better error messages for sort

--- src/main/java/seedu/address/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -1,6 +1,9 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_SORT_FIELD;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_SORT_ORDER;
+import static seedu.address.logic.Messages.MESSAGE_SORT_WRONG_ARGUMENT_COUNT;
 
 import java.util.Locale;
 import java.util.StringTokenizer;
@@ -28,11 +31,13 @@ public class SortCommandParser implements Parser<SortCommand> {
 
         StringTokenizer tokenizer = new StringTokenizer(trimmed);
         if (tokenizer.countTokens() != 2) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+            throw new ParseException(String.format(MESSAGE_SORT_WRONG_ARGUMENT_COUNT, SortCommand.MESSAGE_USAGE));
         }
 
-        String fieldToken = tokenizer.nextToken().toLowerCase(Locale.ROOT);
-        String orderToken = tokenizer.nextToken().toLowerCase(Locale.ROOT);
+        String fieldTokenRaw = tokenizer.nextToken();
+        String orderTokenRaw = tokenizer.nextToken();
+        String fieldToken = fieldTokenRaw.toLowerCase(Locale.ROOT);
+        String orderToken = orderTokenRaw.toLowerCase(Locale.ROOT);
 
         PersonSortField field;
         switch (fieldToken) {
@@ -43,7 +48,7 @@ public class SortCommandParser implements Parser<SortCommand> {
             field = PersonSortField.RATE;
             break;
         default:
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+            throw new ParseException(String.format(MESSAGE_INVALID_SORT_FIELD, fieldTokenRaw));
         }
 
         PersonSortOrder order;
@@ -55,7 +60,7 @@ public class SortCommandParser implements Parser<SortCommand> {
             order = PersonSortOrder.DESCENDING;
             break;
         default:
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+            throw new ParseException(String.format(MESSAGE_INVALID_SORT_ORDER, orderTokenRaw));
         }
 
         return new SortCommand(field, order);
